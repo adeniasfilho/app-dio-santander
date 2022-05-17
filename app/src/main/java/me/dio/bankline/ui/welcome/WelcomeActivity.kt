7503a1ba6c10c514e4f9eb93d4d95344bbf8d842
adeniasfilho <adeniasfilho.n1@gmail.com>
2022-05-16 21:40:03 -0300

    definição do  ícone do app

--- app/src/main/java/me/dio/bankline/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/me/dio/bankline/ui/welcome/WelcomeActivity.kt
@@ -1,12 +1,35 @@
 package me.dio.bankline.ui.welcome
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.dio.bankline.R
+import me.dio.bankline.databinding.ActivityWelcomeBinding
+import me.dio.bankline.domain.Correntista
+import me.dio.bankline.ui.statement.BankStatementActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityWelcomeBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(binding.root)
+
+        binding.btContinue.setOnClickListener {
+            //TODO Melhoria (fácil): validar o preenchimento do ID do Correntista, exibindo um Snackbar em caso de erro.
+            //TODO Melhoria (difícil): evoluir a API para recuperar um Correntista por ID, permitindo assim o envio de mais informações para a próxima tela.
+
+            val accountHolderId = binding.etAccountHolderId.text.toString().toInt()
+            val accountHolder = Correntista(accountHolderId)
+
+            // Reference: https://developer.android.com/training/basics/firstapp/starting-activity
+            val intent = Intent(this, BankStatementActivity::class.java).apply {
+                putExtra(BankStatementActivity.EXTRA_ACCOUNT_HOLDER, accountHolder)
+            }
+            startActivity(intent)
+        }
     }
 }
